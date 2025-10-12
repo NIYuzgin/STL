4239740c4138db399ee391137922f0131d2c4e8d
Николай Немов <prost91@outlook.com>
2025-10-12 12:28:02 +0300

    Sequence containers DONE

---
+++ Police/main.cpp
@@ -0,0 +1,174 @@
+﻿#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<map>
+#include<list>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define delimiter "\n--------------------------------------------\n"
+
+const std::map<int, std::string> VIOLATIONS =
+{
+	{0, "N/A"},
+	{1, "Парковка в неположенном месте"},
+	{2, "Непристегнутый ремень безопасности"},
+	{3, "Пересечение сплошной"},
+	{4, "Превышение скорости"},
+	{5, "Проезд на красный"},
+	{6, "Выезд на встречную полосу"},
+	{7, "Езда в нетрезвом состоянии"},
+	{8, "Оскорбление офицера"},
+};
+class Crime;
+std::stringstream& operator>>(std::stringstream& stream, Crime& obj);
+class Crime
+{
+	int violation;
+	std::string place;
+public:
+	int get_violation()const
+	{
+		return violation;
+	}
+	const std::string& get_place()const
+	{
+		return place;
+	}
+	void set_violation(int violation)
+	{
+		this->violation = violation;
+	}
+	void set_place(const std::string& place)
+	{
+		this->place = place;
+	}
+	Crime(int violation, const std::string& place)
+	{
+		set_violation(violation);
+		set_place(place);
+	}
+	explicit Crime(const std::string& str)
+	{
+		std::stringstream stream(str);
+		stream >> *this;
+	}
+};
+std::ostream& operator<<(std::ostream& os, const Crime& obj)
+{
+	os.width(44);
+	os << std::left;
+	return os << VIOLATIONS.at(obj.get_violation()) << obj.get_place();
+}
+std::ofstream& operator<<(std::ofstream& ofs, const Crime& obj)
+{
+	ofs << obj.get_violation() << " " << obj.get_place();
+	return ofs;
+}
+std::stringstream& operator>>(std::stringstream& stream, Crime& obj)
+{
+	int violation;
+	stream >> violation;
+	std::string place;
+	std::getline(stream, place);
+	obj.set_violation(violation);
+	obj.set_place(place);
+	return stream;
+}
+
+void print(const std::map<std::string, std::list<Crime>>& base);
+void save(const std::map<std::string, std::list<Crime>>& base, const std::string& filename);
+std::map<std::string, std::list<Crime>> load(const std::string& filename);
+
+//#define INIT_BASE
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef INIT_BASE
+	std::map<std::string, std::list<Crime>> base =
+	{
+		{"a123bb", {Crime(2, "ул. Пролетарская"), Crime(3, "ул. Ватутина")}	},
+		{"a777aa", {Crime(4, "ул. Ленина"), Crime(5, "ул. Ленина"), Crime(7, "ул. Энтузиастов"), Crime(8, "ул. Энтузиастов")} },
+		{"a001eg", {Crime(5, "ул. Октябрьская"), Crime(5, "ул. Октябрьская"), Crime(7, "ул. Космическая"), Crime(8, "ул. Энтузиастов")} }
+	};
+	print(base);
+	save(base, "base.txt");
+#endif // INIT_BASE
+
+	std::map<std::string, std::list<Crime>> base = load("base.txt");
+	print(base);
+}
+
+void print(const std::map<std::string, std::list<Crime>>& base)
+{
+	for (std::map<std::string, std::list<Crime>>::const_iterator plate = base.begin(); plate != base.end(); ++plate)
+	{
+		cout << plate->first << ":\n";
+		for (std::list<Crime>::const_iterator violation = plate->second.begin(); violation != plate->second.end(); ++violation)
+		{
+			cout << "\t" << *violation << endl;
+		}
+		cout << delimiter << endl;
+	}
+}
+void save(const std::map<std::string, std::list<Crime>>& base, const std::string& filename)
+{
+	std::ofstream fout(filename);
+	for (std::map<std::string, std::list<Crime>>::const_iterator plate = base.begin(); plate != base.end(); ++plate)
+	{
+		fout << plate->first << ":";
+		for (std::list<Crime>::const_iterator violation = plate->second.begin(); violation != plate->second.end(); ++violation)
+		{
+			fout << *violation << ",";
+		}
+		fout << endl;
+	}
+	fout.close();
+	std::string cmd = "notepad ";
+	cmd += filename;
+	system(cmd.c_str());
+}
+std::map<std::string, std::list<Crime>> load(const std::string& filename)
+{
+	std::map<std::string, std::list<Crime>> base;
+	std::ifstream fin(filename);
+	if (fin.is_open())
+	{
+		while (!fin.eof())
+		{
+			std::string licence_plate;
+			std::getline(fin, licence_plate, ':');
+			cout << licence_plate << "\t";
+			const int SIZE = 1024 * 500;
+			char all_crimes[SIZE];
+			fin.getline(all_crimes, SIZE);
+			cout << all_crimes << endl;
+			const char delimiters[] = ",";
+			
+			
+			for (char* pch = strtok(all_crimes, delimiters); pch; pch = strtok(NULL, delimiters))
+				base[licence_plate].push_back(Crime(pch));
+				
+			
+
+			{
+				/*Crime crime(0,"");
+				std::stringstream stream(pch);
+				stream >> crime;
+				base[licence_plate].push_back(crime);*/
+			}
+		
+
+		}
+	}
+	else
+	{
+		std::cerr << "Error: File not found" << endl;
+	}
+	fin.close();
+	return base;
+}
--- SequenceContainers/main.cpp
+++ SequenceContainers/main.cpp
@@ -4,6 +4,9 @@
 #include<array>
 #include<vector>
 #include<deque>
+#include<list>
+#include<forward_list>
+#include<iterator>
 
 //using namespace std;
 
@@ -15,22 +18,22 @@ using std::endl;
 #define delimiter "\n------------------------------\n"
 
 template<typename T> void vector_info(const std::vector<T>& vec);
-
-
+template<typename T> void deque_info(const std::deque<T>& vec);
 
 //#define STL_ARRAY
-//#define STL_VECTOR
-
-
-
+#define STL_VECTOR
+#define STL_DEQUE
+//#define STL_LIST
+//#define STL_FORWARD_LIST
 
 void main() {
 	setlocale(LC_ALL, "");
 	
 #ifdef STL_ARRAY
+	//array - это контейнер, который хранит данные в виде статического массива;
 	//throw;
 	const int n = 5;
-	std::array<int, n> arr;
+	std::array<int, n > arr;
 	for (int i = 0; i < arr.size(); i++) {
 		arr[i] = rand() % 100;
 	}
@@ -69,19 +72,17 @@ void main() {
 
 #endif STL_ARRAY
 
-
 #ifdef STL_VECTOR
 
 	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
 	for (int i = 0; i < vec.size(); i++) {
-
 		cout << vec[i] << tab;
 	}
 	cout << endl;
 
-
 	vector_info(vec);
 	vec.push_back(55);
+	
 	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
 		cout << *it << tab;
 	cout << endl;
@@ -90,25 +91,28 @@ void main() {
 		cout << *it << tab;
 	cout << endl;
 
-	//
 	vector_info(vec);
 	
 	//vec.shrink_to_fit(); // урезает capacity до фактического размера
-	
-	//vec.reserve(1600);
+	vec.reserve(14);
 	//vec.push_back(1024);
-	vec.resize(14);
+	
+	//vec.resize(18);
 	for (int i : vec)cout << i << tab; cout << endl;
 	vector_info(vec);
+	
 	cout << vec.front() << endl;
 	cout << vec.back() << endl;
-
-
+	
+	//////////////////////////////////////
+	
 	int index;
 	int value;
-	cout << "Введите индекс добавляемого элемента: "; cin >> index;
-	cout << "Введите значение добавляемого элемента: "; cin >> value;
-	vec.insert(vec.begin() + index, value );
+	do {
+		cout << "Введите индекс добавляемого элемента: "; cin >> index;
+		cout << "Введите значение добавляемого элемента: "; cin >> value;
+	} while (index > vec.capacity());
+	vec.insert(vec.begin() + index, value);
 	for (int i : vec)cout << i << tab; cout << endl;
 
 	do {
@@ -116,39 +120,135 @@ void main() {
 
 	} while (index > vec.capacity());
 	vec.erase(vec.begin() + index);
+	for (int i : vec)cout << i << tab; cout << endl;
+	
+#endif STL_VECTOR
 
-
-
-
-
-
-#endif // STL_VECTOR
-
+#ifdef STL_DEQUE
+	// deque - контейнер, который хранит данные в виде списка Векторов.
+	// Это позволяет обеспечить относительно быстрый доступ к элементам, и
+	// относительно быстрое удаление элементов.
+	// deque - Double-ended queue (двунаправленная очередь).
+	// deque по своим возможностям очень напоминает vector, и дополняет его
+	// методами push_front и pop_front.
+	// В то время как Вектор гарантированно хранит данные в одном массиве,
+	// для изменения размера которого его нужно переопределить,
+	// элементы deque могут быть разбросаны в памяти. Именно поэтому Вектор
+	// предоставляет прямой доступ к хранилищу в виде указателя на массив, а
+	// deque этого не делает.
+	// deque внутренне устроен сложнее, чем Вектор, но это позволяет ему изменять
+	// размер более эффективно при тех же обстоятельствах, что и Вектор,
+	// особенно при больших размерах.
 
 	std::deque<int> deque = { 3, 5, 8, 13, 21 };
 	deque.push_back(34);
 	deque.push_front(2);
+	
+	
+	/*
+	std::vector <int> vec;
+	cout << typeid(vec.data()).name() << endl;
+	*/
+	
+	deque_info (deque);
+	deque.assign(vec.begin()+3, vec.end()-3);
+	for (int i : deque)cout << i << tab; cout << endl;
 
 
-	//deque_info (deque);
+#endif STL_DEQUE
 
+#ifdef STL_LIST
+	// list - это контейнер, который хранит данные в виде двусвязного списка, является шаблонным классом
+	std::list<int> list = { 3, 5, 8, 13, 21, 34, 55, 89 };
+	list.push_front(2);
+	list.push_front(1);
+	list.push_front(1);
+	list.push_front(0);
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+		cout << *it << tab;
+		cout << endl;
+	for (std::list<int>::reverse_iterator it = list.rbegin(); it != list.rend(); ++it)
+		cout << *it << tab;
+		cout << endl;
 
+		int index;
+		int value;
+		
+		cout << "Введите индекс добавляемого элемента: "; cin >> index;
+		cout << "Введите значение добавляемого элемента: "; cin >> value;
+		//list.insert(list.begin() + index, value);	// списки не поддерживают арифметику указателей
+		std::list<int>::iterator position = list.begin();
+		//for (int i = 0; i < index; i++) position++;
+		//std::advance(position, index);	// функция advance() смещает итератор вправо
+										// на заданное количество элементов
+		//list.insert(position, value);
+
+		std::list<int>::reverse_iterator r_position = list.rbegin();
+		std::advance(r_position, list.size() - index);
+		position = r_position.base();
+		list.insert(position, value);
+		for (int i : list)cout << i << tab; cout << endl;
+
+		cout << "Введите индекс удаляемого элемента: "; cin >> index;
+		position = list.begin();
+		std::advance(position, index);
+		list.erase(position);
+		for (int i : list)cout << i << tab; cout << endl;
+		
+#endif STL_LIST
+
+#ifdef STL_FORWARD_LIST
+
+		std::forward_list<int> list = { 3, 5, 8, 13, 21 };
+		list.push_front(2);
+		list.push_front(1);
+		list.push_front(1);
+		list.push_front(0);
+		for(std::forward_list<int>::iterator it = list.begin();it!=list.end();++it)
+			cout << *it << tab;
+		cout << endl;
+
+		int index;
+		int value;
+		cout << "Введите индекс добавляемого элемента: "; cin >> index;
+		cout << "Введите значение добавляемого элемента: "; cin >> value;
+		std::forward_list<int>::iterator position = list.before_begin();
+		std::advance(position, index);
+		list.insert_after(position, value);
+		for (int i : list)cout << i << tab; cout << endl;
+
+		cout << "Введите индекс удаляемого элемента: "; cin >> index;
+		position = list.begin();
+		std::advance(position, index - 1);
+		list.erase_after(position);
+		for (int i : list)cout << i << tab; cout << endl;
+
+#endif STL_FORWARD_LIST
 
 }
 
 template<typename T> void vector_info(const std::vector<T>& vec) {
 
-cout << "Size:\t" << vec.size() << endl; // Фактический размер Вектора, показывающий 
-	cout << "MaxSize:\t" << vec.max_size() << endl;
-	cout << "Capacity:\t" << vec.capacity() << endl; // Объем зарезервированной памяти. Зарезервированная 
-	// память нужна для того, чтобы ускорить процесс добавления элементов и избежать необходимости
-	// переопределять память при каждом добавлении элемента. Это экономит ресурсы процессорного времени,
-	// но расходует дополнительные ресурсы
-	// Если у Вектора есть свободная зарезервированная память, то добавляемое значение
-	// но если зарезервированная память закончилась, то при добавлении элемента Вектор резервирует
-	// в половину больше фактического размера.
-	// На capacity можно повлиять при помощи методов reserve(), resize(), и shrink_to_fit()
+	cout << "Size:\t " << vec.size() << endl; // Фактический размер Вектора, 
+	// показывающий сколько элементов хранит Вектор
+	cout << "MaxSize: " << vec.max_size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl; // Объем зарезервированной памяти.
+	// Зарезервированная память нужна для того, чтобы ускорить процесс добавления элементов
+	// и избежать необходимости переопределять память при каждом добавлении элемента. 
+	// Это экономит ресурсы процессорного времени, но расходует дополнительные ресурсы памяти.
+	// Если у Вектора есть свободная зарезервированная память, то добавляемое значение 
+	// просто записывается в конец массива, но если зарезервированная память закончилась,
+	// то при добавлении элемента Вектор резервирует новую память в половину больше фактического размера.
+	// На capacity можно повлиять при помощи методов reserve(), resize(), и shrink_to_fit().
 
 	cout << delimiter << endl;
-
 }
+
+template<typename T> void deque_info(const std::deque<T>& vec) {
+	cout << "Size:\t " << vec.size() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
+	//cout << "Capacity: " << vec.capacity() << endl;
+	cout << delimiter << endl;
+}
+
+
